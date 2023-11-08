d0cdd4af8af2d74baf902d70fbc8a9e6351e2c1c
Marcus Munch Grünewald <MMGR@SSI.dk>
2023-11-08 13:29:32 +0100

    Call Rscript in print-args.steps

--- print_args.R
+++ print_args.R
@@ -1,8 +1,12 @@
-args <- commandArgs(trailingOnly = TRUE)
+load_backend <- function(backend_name) {
+  backend_functions <- list(
+    "Postgres" = "RPostgres::Postgres",
+    "RSQLite" = "RSQLite::SQLite"
+  )
 
-if (length(args) > 0) print(args)
+  return(backend_functions[[backend_name]])
+}
 
-env <- Sys.getenv()
+bck <- Sys.getenv("MY_BACKEND")
 
-cat("Try to get backend")
-print(env[grepl("backend", names(env), ignore.case = TRUE)])
+cat("Driver to use for ", bck, " is ", load_backend(bck), "()\n", sep  = "")
