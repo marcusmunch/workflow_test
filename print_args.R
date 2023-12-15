1fefed9c78b42795ddd23dea1c9dcd513e006861
Marcus Munch Grünewald <MMGR@SSI.dk>
2023-12-15 11:06:04 +0100

    Sort env, args as string

--- print_args.R
+++ print_args.R
@@ -7,7 +7,7 @@ drv <- env[grepl("drv$", names(env), ignore.case = TRUE)]
 args <- env[grepl("args$", names(env), ignore.case = TRUE)]
 
 message("env:")
-print(env)
+print(env[sort.list(names(env))])
 message("---")
 
 message("drv:")
