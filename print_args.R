2a5b5dff01c4167c881fa9e9e9bd7e08e6462fd2
Marcus Munch Grünewald <MMGR@SSI.dk>
2023-12-15 11:12:53 +0100

    args as named list

--- print_args.R
+++ print_args.R
@@ -4,7 +4,7 @@ if (!requireNamespace("jsonlite", quietly = TRUE)) install.packages("jsonlite")
 env <- Sys.getenv()
 
 drv <- env[grepl("drv$", names(env), ignore.case = TRUE)]
-args <- env[grepl("args$", names(env), ignore.case = TRUE)]
+args <- env[grepl("arg_list$", names(env), ignore.case = TRUE)]
 
 message("env:")
 print(env[sort.list(names(env))])
